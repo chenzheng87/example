82d745fc99cbdf7203243bf7ee0046ffaba87123
zhenglaobao <zhenglaobao@sina.com>
2015-12-29 16:19:15 +0800

    add timer feature

---
+++ src/timer.h
@@ -0,0 +1,27 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <pthread.h>
+#include <sys/time.h>
+class CTimer
+{
+   private:
+    pthread_t thread_timer;
+    long m_second, m_microsecond;
+    static void *OnTimer_stub(void *p)
+    {
+	        (static_cast<CTimer*>(p))->thread_proc();
+			return (void *)0;
+    }
+    void thread_proc();
+    void OnTimer();
+   public:
+    CTimer();
+    CTimer(long second, long microsecond);
+    virtual ~CTimer();
+    void SetTimer(long second,long microsecond);
+    void StartTimer();
+    void StopTimer();
+};
+
+#endif
