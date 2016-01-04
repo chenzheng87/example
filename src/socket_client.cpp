e8cfa979889428bd21a48536014886a1d508ed54
zhenglaobao <zhenglaobao@sina.com>
2016-01-04 22:59:06 +0800

    daily update

--- src/socket_client.cpp
+++ src/socket_client.cpp
@@ -36,36 +36,106 @@ int main(int argc,char *argv[])
 	}  
 
 	int arg_cnt = 0;
-	char arg_str[4] = {0};	
-	
+	char arg_str[100] = {0};	
+
 	int ret_val;
-	fd_set wfds;
+	fd_set fds;
 	int maxfd = -1;
 	struct timeval time_limit;
 	time_limit.tv_sec = 1;
 	time_limit.tv_usec = 0;
+	//send data example
+   /* while(true){*/
+		//FD_ZERO(&fds);
+		//FD_SET(0, &fds);
+		//maxfd = 0;
+		//FD_SET(sockfd, &fds);
+		//if(sockfd > maxfd)	
+			//maxfd = sockfd;
+		//ret_val = select(maxfd + 1, NULL, &fds, NULL, &time_limit);
+		//switch(ret_val){
+			//case -1:
+				////select error
+			//case 0:
+				////time out
+				//close(sockfd);	
+				//if((sockfd = socket(AF_INET,SOCK_STREAM,0)) == -1){  
+					//perror("reconnect sock");  
+					//exit(EXIT_FAILURE);  
+				//}
+				//while(connect(sockfd, (struct sockaddr *)&address, len) == -1){
+					//perror("reconnect");
+					//sleep(1);
+				//}
+				//break;
+			//default:
+				////data ready to send
+				//if(FD_ISSET(sockfd, &fds)){
+					//if(arg_cnt == 20)
+						//arg_cnt = 0;
+					//sprintf(arg_str, "%d", arg_cnt++);
+					//if((byte=send(sockfd, arg_str, 4, MSG_NOSIGNAL))==-1){  
+						//perror("send");  
+						//close(sockfd);
+						//if((sockfd = socket(AF_INET,SOCK_STREAM,0)) == -1){  
+							//perror("reconnect sock");  
+							//exit(EXIT_FAILURE);  
+						//}
+						//while(connect(sockfd, (struct sockaddr *)&address, len) == -1){
+							//perror("reconnect connect");
+							//sleep(1);
+						//}
+					//}             
+				//}
+				//break;
+		//}
+		//sleep(1);
+	/*}*/ 
+	//recieve data example
 	while(true){
-		FD_ZERO(&wfds);
-		FD_SET(0, &wfds);
+		FD_ZERO(&fds);
+		FD_SET(0, &fds);
 		maxfd = 0;
-		FD_SET(sockfd, &wfds);
+		FD_SET(sockfd, &fds);
 		if(sockfd > maxfd)	
 			maxfd = sockfd;
-		ret_val = select(maxfd + 1, NULL, &wfds, NULL, &time_limit);
-		if(ret_val < 1){
-			close(sockfd);
-			//reconnect
-			while(connect(sockfd, (struct sockaddr *)&address, len) == -1)	
-				perror("reconnect");
-		}
-		if(FD_ISSET(sockfd, &wfds)){
-			if(arg_cnt == 20)
-				arg_cnt = 0;
-			sprintf(arg_str, "%d", arg_cnt++);
-			if((byte=send(sockfd, arg_str, 4, 0))==-1){  
-				perror("send");  
-				exit(EXIT_FAILURE);  
-			}             
+		ret_val = select(maxfd + 1, &fds, NULL, NULL, &time_limit);
+		switch(ret_val){
+			case -1:
+				//select error
+				close(sockfd);	
+				if((sockfd = socket(AF_INET,SOCK_STREAM,0)) == -1){  
+					perror("reconnect sock");  
+					exit(EXIT_FAILURE);  
+				}
+				while(connect(sockfd, (struct sockaddr *)&address, len) == -1){
+					perror("reconnect");
+					sleep(1);
+				}
+				break;
+			case 0:
+				//time out, try again
+				break;
+			default:
+				//data ready to send
+				if(FD_ISSET(sockfd, &fds)){
+					if(arg_cnt == 20)
+						arg_cnt = 0;
+					sprintf(arg_str, "%d", arg_cnt++);
+					if((byte=recv(sockfd, arg_str, 100, MSG_NOSIGNAL))==0){  
+						perror("recieve");  
+						close(sockfd);
+						if((sockfd = socket(AF_INET,SOCK_STREAM,0)) == -1){  
+							perror("reconnect sock");  
+							exit(EXIT_FAILURE);  
+						}
+						while(connect(sockfd, (struct sockaddr *)&address, len) == -1){
+							perror("reconnect connect");
+							sleep(1);
+						}
+					}             
+				}
+				break;
 		}
 		sleep(1);
 	}  
